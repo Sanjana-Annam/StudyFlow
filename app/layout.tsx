613a68dd74e355217cff3060e9ff2ff9ed1f1c37
sanjana-Annam <sanjanaannam87@gmail.com>
2026-05-24 18:13:58 +0530

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/icon.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
